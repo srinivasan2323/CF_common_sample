516e1bf0e60962ec084f3f1e533c31543c2cc725
adinarayana.hosuru <adinarayana.hosuru@BDC3-D-2WST6BS.dir.svc.accenture.com>
2016-12-23 16:56:26 +0530

    add10

--- src/main/java/org/cloudfoundry/samples/music/config/data/MongoConfig.java
+++ src/main/java/org/cloudfoundry/samples/music/config/data/MongoConfig.java
@@ -20,13 +20,13 @@ public class MongoConfig {
     public String empname7;
 
     
-    private int empid;
-    private int empid7;
-
+    private int empid;  
+    private int emp_no;
+    
 System.out.println("eqjmnofdhn");
 private int studentid7;
 
-private int add7;
+private int add10;
 
 
 
